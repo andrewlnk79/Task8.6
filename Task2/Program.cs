5e14db60a38f3730ac3234cd886f5552c50ad27b
Andrey Linnik <av.linnik79@ya.ru>
2023-03-24 15:12:43 +0300

    finish task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,28 @@
 ﻿//Напишите программу, которая считает размер папки на диске (вместе со всеми вложенными папками и файлами).
 //На вход метод принимает URL директории, в ответ — размер в байтах.
+GetDirectoryInfo(@"D:\Texpress",out long Size);
+Console.WriteLine(Size);
+Console.ReadKey();
+
+
+static void GetDirectoryInfo(string path, out long Size)
+{
+    Size = 0;
+    DirectoryInfo directoryInfo = new DirectoryInfo(path);
+    var subDirectories = directoryInfo.GetDirectories();
+    var files = directoryInfo.GetFiles();
+    foreach (var file in files)
+    {
+        Size += file.Length;
+    }
+    long DirSize = 0;
+    foreach (var dir in subDirectories)
+    {
+        GetDirectoryInfo(dir.FullName, out DirSize);
+        Size += DirSize;
+    }
+    ;
+
+
+}
+
